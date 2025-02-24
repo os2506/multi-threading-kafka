30e83c60494a95a95930fcedb90cb46c9a396df5
O&S <said-souilem@live.fr>
2025-02-24 09:24:06 +0100

    Update PaymentEventRepository.java

--- src/main/java/com/oss/demo/repository/PaymentEventRepository.java
+++ src/main/java/com/oss/demo/repository/PaymentEventRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.oss.demo.eventModel.PaymentEvent;
 
 
-
 public interface PaymentEventRepository extends JpaRepository<PaymentEvent, Long> {
-    // You can add custom query methods if needed, e.g., find by orderId
     PaymentEvent findByPurchaseId(Long purchaseId);
 }
