cad3da0821732e9efaf5b84238ab75fb8a266a27
O&S <said-souilem@live.fr>
2025-02-24 09:20:11 +0100

    Update OrderEventProducer.java

--- src/main/java/com/oss/demo/service/producer/OrderEventProducer.java
+++ src/main/java/com/oss/demo/service/producer/OrderEventProducer.java
@@ -3,7 +3,6 @@ package com.oss.demo.service.producer;
 
 import com.oss.demo.eventModel.OrderEvent;
 import com.oss.demo.repository.OrderEventRepository;
-
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,24 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderEventProducer {
 
-    private static final String TOPIC = "order_events"; // Your Kafka topic
+    private static final String TOPIC = "order_events";
     
-
     @Autowired
     private OrderEventRepository orderEventRepository;
     
-    
-
     @Autowired
     private KafkaTemplate<String, OrderEvent> kafkaTemplate;
 
     public void sendOrderEvent(OrderEvent orderEvent) {
-        kafkaTemplate.send(TOPIC, orderEvent); // Send event to Kafka
+        kafkaTemplate.send(TOPIC, orderEvent); 
     }
     
-   
     public void saveOrderEvent(OrderEvent orderEvent) {
-        // Save to the database
         orderEventRepository.save(orderEvent);
     }
    
